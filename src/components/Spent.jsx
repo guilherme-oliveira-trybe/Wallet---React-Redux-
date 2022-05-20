16eadb23a057ecc34f54283910da7c6cb985dca6
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-05-20 10:13:34 -0300

    Requisito 8

--- src/components/Spent.jsx
+++ src/components/Spent.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 
 class Spent extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -18,13 +20,58 @@ class Spent extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          {expenses.map((spent) => (
+            <tr key={ spent.id }>
+              <td>
+                {' '}
+                { spent.description }
+                {' '}
+              </td>
+              <td>
+                {' '}
+                { spent.tag }
+                {' '}
+              </td>
+              <td>
+                {' '}
+                {spent.method}
+                {' '}
+              </td>
+              <td>
+                {' '}
+                { Number(spent.value).toFixed(2) }
+                {' '}
+              </td>
+              <td>
+                {' '}
+                { spent.exchangeRates[spent.currency].name }
+                {' '}
+              </td>
+              <td>
+                {Number(spent.exchangeRates[spent.currency].ask).toFixed(2)}
+              </td>
+              <td>
+                {' '}
+                { Number(spent.value * spent.exchangeRates[spent.currency].ask)
+                  .toFixed(2)}
+                {' '}
+              </td>
+              <td> Real </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({
-//   expenses: state.wallet.expenses,
-// });
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
 
-export default connect()(Spent);
+Spent.propTypes = {
+  expenses: propTypes.arrayOf(propTypes.array),
+}.isRequired;
+
+export default connect(mapStateToProps)(Spent);
